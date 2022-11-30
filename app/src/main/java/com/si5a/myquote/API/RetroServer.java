0ee7401e5e60c921d464c22d8847d0055e30005b
wilsen <clarameyhazlinda@mhs.mdp.ac.id>
2022-11-30 16:57:52 +0700

    update egen

---
+++ app/src/main/java/com/si5a/myquote/API/RetroServer.java
@@ -0,0 +1,8 @@
+package com.si5a.myquote.API;
+
+import retrofit2.Retrofit;
+
+public class RetroServer {
+    private static final String baseURL = "http://type.fit/api/";
+    private static Retrofit retro;
+}
